cb5b3462a856bca48b04dd31ab0d3321154b343c
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-11-06 17:54:49 -0800

    Use a single language service instance, provide semantic classifications, support .js files.
    Previously using one language service per replace would take 10 seconds
    to render 'Basics.md'; now it's closer to 4 seconds.
    
    Semantic classifications are now interleaved with syntactic
    classifications.
    
    We now support .tsx, .jsx, and .js as extensions. Keep an eye out for
    .json eventually.

--- scripts/render.ts
+++ scripts/render.ts
@@ -9,17 +9,17 @@ function cleanMarkdownEscaped(code: string) {
     return code;
 }
 
-const sampleFileName = "sample.ts";
-function createLanguageServiceHost(content: string): ts.LanguageServiceHost {
+function createLanguageServiceHost(ref: SampleRef): ts.LanguageServiceHost {
     const options: ts.CompilerOptions = {
-
+        allowJs: true,
+        skipLibCheck: true,
     };
     const servicesHost: ts.LanguageServiceHost = {
-        getScriptFileNames: () => [sampleFileName],
-        getScriptVersion: fileName => "1",
+        getScriptFileNames: () => [ref.fileName!],
+        getScriptVersion: fileName => ref.fileName === fileName ? "" + ref.versionNumber : "0",
         getScriptSnapshot: fileName => {
-            if (fileName === sampleFileName) {
-                return ts.ScriptSnapshot.fromString(content);
+            if (fileName === ref.fileName) {
+                return ts.ScriptSnapshot.fromString(ref.content);
             }
             if (!fs.existsSync(fileName)) {
                 return undefined;
@@ -32,59 +32,86 @@ function createLanguageServiceHost(content: string): ts.LanguageServiceHost {
         getDefaultLibFileName: options => ts.getDefaultLibFilePath(options),
         fileExists: ts.sys.fileExists,
         readFile: ts.sys.readFile,
-        readDirectory: ts.sys.readDirectory
+        readDirectory: ts.sys.readDirectory,
     };
     return servicesHost;
 }
 
+interface SampleRef {
+    fileName: string | undefined;
+    versionNumber: number;
+    content: string
+}
+
 function getCompilerExtension() {
-    const evalFileName = 'input.ts';
     const matches: string[] = [];
     let self = false;
+
+    const sampleFileRef: SampleRef = { fileName: undefined, content: "", versionNumber: 0 };
+    const lsHost = createLanguageServiceHost(sampleFileRef);
+    const caseSensitiveFilenames = lsHost.useCaseSensitiveFileNames && lsHost.useCaseSensitiveFileNames();
+    const docRegistry = ts.createDocumentRegistry(caseSensitiveFilenames, lsHost.getCurrentDirectory());
+    const ls = ts.createLanguageService(lsHost, docRegistry);
+    const compilerOptions = lsHost.getCompilationSettings();
     const ext: showdown.ShowdownExtension[] = [
         {
             type: "lang",
-            regex: /```ts\r?\n([\s\S]*?)\r?\n```/g,
-            replace: function (match: string, code: string) {
+            regex: /```(jsx?|tsx?)\r?\n([\s\S]*?)\r?\n```/g,
+            replace: function (_: string, extension: string, code: string) {
                 code = cleanMarkdownEscaped(code);
-                const src = ts.createSourceFile("input.ts", code, ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
-                const lsHost = createLanguageServiceHost(code);
-                const ls = ts.createLanguageService(lsHost);
-                const spans = ls.getEncodedSyntacticClassifications(sampleFileName, ts.createTextSpan(0, code.length));
-                const errs = ls.getSemanticDiagnostics(sampleFileName);
-                errs.push(...ls.getSyntacticDiagnostics(sampleFileName));
+
+                sampleFileRef.fileName = "input." + extension;
+                sampleFileRef.content = code;
+                sampleFileRef.versionNumber++;
+
+                const scriptSnapshot = lsHost.getScriptSnapshot(sampleFileRef.fileName);
+                const scriptVersion = "" + sampleFileRef.versionNumber;
+                docRegistry.updateDocument(sampleFileRef.fileName, compilerOptions, scriptSnapshot!, scriptVersion);
+
+                const syntaxSpans = ls.getEncodedSyntacticClassifications(sampleFileRef.fileName, ts.createTextSpan(0, code.length)).spans;
+                const semanticSpans = ls.getEncodedSemanticClassifications(sampleFileRef.fileName, ts.createTextSpan(0, code.length)).spans;
+                const errs = ls.getSemanticDiagnostics(sampleFileRef.fileName);
+                errs.push(...ls.getSyntacticDiagnostics(sampleFileRef.fileName));
 
                 const parts: string[] = [`<pre class="typescript-code">`];
-                
-                const currentToken = 0;
-                let pendingDiagnosticEnd = -1;
+
                 for(let i = 0; i < code.length; i++) {
-                    const endingDiags = errs.filter(diag => (diag.file && diag.file.fileName) === sampleFileName && (diag.start! + diag.length! === i));
+                    const endingDiags = errs.filter(diag => (diag.file && diag.file.fileName) === sampleFileRef.fileName && (diag.start! + diag.length! === i));
                     for (const end of endingDiags) {
                         parts.push(`</span>`);
                     }
 
-                    const startingDiags = errs.filter(diag => (diag.file && diag.file.fileName) === sampleFileName && (diag.start === i));
+                    const startingDiags = errs.filter(diag => (diag.file && diag.file.fileName) === sampleFileRef.fileName && (diag.start === i));
                     for (const start of startingDiags) {
                         const messageText = typeof start.messageText === "string" ? start.messageText : start.messageText.messageText;
                         parts.push(`<span class="error" title="${messageText.replace(/"/g, "&gt;")}">`);
                     }
 
-                    if (i === spans.spans[0]) {
-                        const [start, length, kind] = spans.spans;
-                        parts.push(`<span class="${ts.ClassificationType[kind]}">${code.substr(i, length)}</span>`);
-                        spans.spans.shift();
-                        spans.spans.shift();
-                        spans.spans.shift();
-                        i += length - 1;
-                    } else {
+                    if (i === semanticSpans[0]) {
+                        if (i === syntaxSpans[0]) {
+                            // Better hope these overlap the right way lol
+                            if (semanticSpans[1] !== syntaxSpans[1]) {
+                                throw new Error(
+                                    "Semantic and syntactic spans don't overlap. " +
+                                    " Either make this script resilient to that or disable semantic classification."
+                                );
+                            }
+                            syntaxSpans.shift();
+                            syntaxSpans.shift();
+                            syntaxSpans.shift();
+                        }
+                        i = highlightAndBumpCounter(semanticSpans, parts, code, i);
+                    } else if (i === syntaxSpans[0]) {
+                        i = highlightAndBumpCounter(syntaxSpans, parts, code, i);
+                    }
+                    else {
                         parts.push(code.substr(i, 1));
                     }
                 }
                 const url = `https://www.typescriptlang.org/play/#src=${encodeURIComponent(code)}`;
                 parts.push(`<a class="playground-link" href="${url}">Try in Playground</a>`)
                 parts.push("</pre>");
-                
+
                 return "%FCODERENDER" + (matches.push(parts.join("")) - 1) + "%";
             }
         },
@@ -106,10 +133,21 @@ function getCompilerExtension() {
         }
     ];
     return ext;
+
+    function highlightAndBumpCounter(spans: number[], parts: string[], code: string, i: number) {
+        const [start, length, kind] = spans;
+        parts.push(`<span class="${ts.ClassificationType[kind]}">${code.substr(i, length)}</span>`);
+        spans.shift();
+        spans.shift();
+        spans.shift();
+        return i + length - 1;
+    }
 }
 
 export function render(content: string) {
-    const conv = new showdown.Converter();
+    const conv = new showdown.Converter({
+        ghCodeBlocks: true,
+    });
     conv.addExtension(getCompilerExtension(), "ts");
     return conv.makeHtml(content);
 }
@@ -124,7 +162,11 @@ export function makePage(content: string) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" media="screen" href="handbook.css" />
     </head>
-    <body>${content}</body>
+    <body>
+    <article>
+    ${content}
+    </article>
+    </body>
     </html>`;
 }
 
