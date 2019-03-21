7f9238923339254fdd1de58faacd582ee21df273
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2019-03-21 14:15:50 -0700

    Move narrowing

--- scripts/site.ts
+++ scripts/site.ts
@@ -5,12 +5,18 @@ import fs = require('fs-extra');
 import render = require('./render');
 import { Outline } from './master-outline-generator';
 import { fileNameToUrlName } from './utils';
+import { getHeaders } from './header-parser';
 
 const renderMarkdownPage: staticy.TextTransform = {
     changeFileName: fn => `${fileNameToUrlName(fn.replace(/\.md$/, ""))}/index.html`,
     transform(context) {
+        let title: string | undefined;
+        let h1 = getHeaders(context.content).filter(h => h.depth === 1)[0];
+        if (h1 !== undefined) {
+            title = h1.title;
+        }
         return {
-            content: render.makePage(render.render(context.content))
+            content: render.makePage(render.render(context.content), { title })
         };
     }
 }
