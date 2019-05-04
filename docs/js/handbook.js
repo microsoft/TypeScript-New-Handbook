c708baf09d8efaaf0a6c15db7aaf793ea6a7e68d
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2019-05-03 23:53:57 -0700

    Publish

--- docs/js/handbook.js
+++ docs/js/handbook.js
@@ -1,5 +1,10 @@
+// @ts-check
+
 var themes = ["dark", "dark-light", "light-dark", "light"];
 document.addEventListener("DOMContentLoaded", function() {
+    if (typeof window.localStorage !== "object" || !window.localStorage) {
+        return;
+    }
     var savedTheme = window.localStorage.getItem("handbook-theme");
     var ti = themes.indexOf(savedTheme);
     if (ti >= 0) {
@@ -10,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         listen(themes[i]);
     }
 
+    /** @param {string} theme */
     function listen(theme) {
         document.getElementById("set-theme-" + theme).addEventListener("click", function() {
             setTheme(theme);
@@ -17,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    /** @param {string} theme */
     function setTheme(theme) {
         for (var i = 0; i < themes.length; i++) {
             document.body.classList.remove(themes[i]);
